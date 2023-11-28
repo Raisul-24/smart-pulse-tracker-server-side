132d9a4aaaae6639435c8ddeb0ccc788fa7e6e07
Raisul Islam Hridoy <raisul15-3813@diu.edu.bd>
2023-11-28 07:26:29 +0600

    add payment intent

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 // const jwt = require('jsonwebtoken');
 
 // This is your test secret API key.
-// const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 const app = express()
 
@@ -48,6 +48,12 @@ app.use(express.json());
 // }
 
 
+const calculateOrderAmount = (items) => {
+   // Replace this constant with a calculation of the order's amount
+   // Calculate the order total on the server to prevent
+   // people from directly manipulating the amount on the client
+   return 1400;
+ };
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.sxdrhxr.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -76,6 +82,8 @@ const appliedTrainerCollection = client.db('SmartPulse-Fitness-Tracker').collect
 const trainerBookCollection = client.db('SmartPulse-Fitness-Tracker').collection('bookings');
 const subscribersCollection = client.db('SmartPulse-Fitness-Tracker').collection('subscribers');
 const postCollection = client.db('SmartPulse-Fitness-Tracker').collection('forum');
+const paymentCollection = client.db('SmartPulse-Fitness-Tracker').collection('payments');
+
 
 
 // user related api
@@ -190,6 +198,28 @@ app.patch('/forum/:id/vote', async (req, res) => {
    const result = await postCollection.findOne(filter);
    res.send(result);
 });
+
+// payment related api
+// payment intent
+ 
+app.post("/create-payment-intent", async (req, res) => {
+   const { items } = req.body;
+ 
+   // Create a PaymentIntent with the order amount and currency
+   const paymentIntent = await stripe.paymentIntents.create({
+     amount: calculateOrderAmount(items),
+     currency: "usd",
+     // In the latest version of the API, specifying the `automatic_payment_methods` parameter is optional because Stripe enables its functionality by default.
+     automatic_payment_methods: {
+       enabled: true,
+     },
+   });
+ 
+   res.send({
+     clientSecret: paymentIntent.client_secret,
+   });
+ });
+
 
 
 app.get('/', (req, res) => {

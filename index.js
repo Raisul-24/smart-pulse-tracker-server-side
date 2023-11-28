863f35588ddc6997a8f28d3958ad376849a1ee22
Raisul Islam Hridoy <raisul15-3813@diu.edu.bd>
2023-11-28 16:20:10 +0600

    jwt and verify token

--- index.js
+++ index.js
@@ -3,13 +3,12 @@ const express = require('express');
 
 const cors = require('cors');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-// const jwt = require('jsonwebtoken');
+const jwt = require('jsonwebtoken');
 
 // This is your test secret API key.
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 const app = express()
-
 const port = process.env.PORT || 5013
 
 // middleware
@@ -17,35 +16,34 @@ app.use(cors());
 app.use(express.json());
 
 
-// // middleware
-// const verifyToken = (req, res, next) => {
-//    console.log('inside verify token', req.headers);
-//    if (!req.headers.authorization) {
-//       return res.status(401).send({ message: 'forbidden access' })
-//    }
-//    const token = req.headers.authorization.split(' ')[1];
-//    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
-//       if (err) {
-//          return res.status(401).send({ message: 'forbidden access' })
-//       }
-//       req.decoded = decoded;
-//       next();
-//    })
 
-// }
+// middleware
+const verifyToken = (req, res, next) => {
+   console.log('inside verify token', req.headers);
+   if (!req.headers.authorization) {
+      return res.status(401).send({ message: 'forbidden access' })
+   }
+   const token = req.headers.authorization.split(' ')[1];
+   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+      if (err) {
+         return res.status(401).send({ message: 'forbidden access' })
+      }
+      req.decoded = decoded;
+      next();
+   })
 
-
+}
 // use verify admin after verifyToken
-// const verifyAdmin = async (req, res, next) => {
-//    const email = req.decoded.email;
-//    const query = { email: email };
-//    const user = await userCollection.findOne(query);
-//    const isAdmin = user?.role === 'admin';
-//    if (!isAdmin) {
-//       return res.status(403).send({ message: 'forbidden access' });
-//    }
-//    next();
-// }
+const verifyAdmin = async (req, res, next) => {
+   const email = req.decoded.email;
+   const query = { email: email };
+   const user = await userCollection.findOne(query);
+   const isAdmin = user?.role === 'admin';
+   if (!isAdmin) {
+      return res.status(403).send({ message: 'forbidden access' });
+   }
+   next();
+}
 
 
 const calculateOrderAmount = (items) => {
@@ -84,14 +82,39 @@ const subscribersCollection = client.db('SmartPulse-Fitness-Tracker').collection
 const postCollection = client.db('SmartPulse-Fitness-Tracker').collection('forum');
 const paymentCollection = client.db('SmartPulse-Fitness-Tracker').collection('payments');
 
-
-
-// user related api
-app.get('/users', async (req, res) => {
-   const result = await userCollection.find().toArray();
-   res.send(result)
+// jwt related api
+app.post('/jwt', async (req, res) => {
+   const user = req.body;
+   const token = await jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+      expiresIn: '1h'
+   });
+   res.send({ token });
 });
 
+// user related api
+app.get('/users',verifyToken, verifyAdmin, async (req, res) => {
+   const result = await userCollection.find().toArray();
+   res.send(result);
+});
+
+// admin verify
+app.get('/users/admin/:email', verifyToken, async (req, res) => {
+   const email = req.params.email;
+   if (email !== req.decoded.email) {
+      return res.status(403).send({ message: 'unauthorized access' })
+   }
+
+   const query = { email: email }
+   const user = await userCollection.findOne(query);
+   let admin = false;
+   if (user) {
+      admin = user?.role === 'admin';
+   }
+   res.send({ admin })
+});
+
+
+// post user
 app.post('/users', async (req, res) => {
    const user = req.body;
    // insert email if user doesn't exists.
@@ -154,6 +177,18 @@ app.get('/trainers/:id', async (req, res) => {
    const result = await trainerCollection.findOne(query);
    res.send(result);
 });
+// get status
+app.patch('/trainers/:id', async (req, res) => {
+   const id = req.params.id;
+   const filter = { _id: new ObjectId(id) }
+   const updatedDoc = {
+      $set: {
+         role: 'paid'
+      }
+   }
+   const result = await trainerCollection.updateOne(filter, updatedDoc);
+   res.send(result);
+   });
 // user book trainer
 app.post('/bookings', async (req, res) => {
    const bookInfo = req.body;
@@ -204,7 +239,7 @@ app.patch('/forum/:id/vote', async (req, res) => {
  
 app.post("/create-payment-intent", async (req, res) => {
    const { items } = req.body;
- 
+ console.log(items)
    // Create a PaymentIntent with the order amount and currency
    const paymentIntent = await stripe.paymentIntents.create({
      amount: calculateOrderAmount(items),
@@ -219,7 +254,22 @@ app.post("/create-payment-intent", async (req, res) => {
      clientSecret: paymentIntent.client_secret,
    });
  });
-
+// specific email holder history
+app.get('/payments/:email', async (req, res) => {
+   const query = { email: req.params.email }
+   if (req.params.email !== req.decoded.email) {
+      return res.status(403).send({ message: 'forbidden access' });
+   }
+   const result = await paymentCollection.find(query).toArray();
+   res.send(result);
+});
+// payment info store delete
+app.post('/payments', async (req, res) => {
+   const payment = req.body;
+   const paymentResult = await paymentCollection.insertOne(payment);
+   console.log('payment info', payment);
+   res.send({ paymentResult });
+});
 
 
 app.get('/', (req, res) => {
